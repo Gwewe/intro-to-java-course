dac80f2531eb324070683ca80821bf422c20278d
Wedna Guirand <wedna.guirand@gmail.com>
2024-03-27 20:41:10 +0000

    created hello world java code

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+package com.codingblackfemales.academu;
+
+public class HelloWorld {
+    public static void main(String... args) {
+        system.System.out.println("Hello World! It's Wednesday Yay!");
+    }
+}
