7b5e6e9f57ad004be26e313a46b74a0cd16d5700
Wedna Guirand <wedna.guirand@gmail.com>
2024-04-18 19:51:32 +0100

    completed first part of Account exercices

---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/accounts/Account.java
@@ -0,0 +1,48 @@
+package com.cbfacademy.accounts;
+
+public class Account {
+    int accountNumber;
+    double balance;
+
+    public Account(int accountNumber, double balance){
+            //constructor that accepts parameters representing the new account number and starting balance
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+    } 
+    
+    
+    
+    public double getBalance(){
+        //returns the current account balance
+        return this.balance;
+    } 
+    
+    
+    
+    public int getAccountNumber(){
+        // returns the account number
+        return this.accountNumber;
+    }
+    
+    
+    
+    
+    public double deposit(double amount){
+        //deposits funds to the account and returns the new balance
+        this.balance = balance + amount;
+        return balance;
+    }
+    
+    
+    
+    public double withdraw(double requested){
+       // withdraws funds from the account and returns the requested amount or 0 if the account has an insufficient balance
+       if (requested > 0 && balance > requested) {
+            balance -= requested;
+            return requested;
+       } else {
+            return 0;
+        
+       }
+    }
+}
